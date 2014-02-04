00aee667334d66ae062be874f34ab195503ab448
Johnny Loi <theaznshumai@gmail.com>
2014-02-03 23:16:05 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SkillMatrix::Application.config.secret_key_base = 'c54a920f4a5fb8c1d8d4db53aad4d2d88ef497cb7e53b9bffb49fdbea91ae735556ab3f0806f808c8937af4a8600267f0ca971fc9787eb8564e212c023d25670'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SkillMatrix::Application.config.session_store :cookie_store, key: '_skillMatrix_session'
