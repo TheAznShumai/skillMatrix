9969e7320fdb749ba6168eeaebf8c98107c5b978
Johnny Loi <theaznshumai@gmail.com>
2014-02-04 22:46:42 -0800

    Rateable_skills

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140205025724) do
+ActiveRecord::Schema.define(version: 20140205062859) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,12 @@ ActiveRecord::Schema.define(version: 20140205025724) do
   create_table "skills", force: true do |t|
     t.string   "name"
     t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "surveys", force: true do |t|
+    t.integer  "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
