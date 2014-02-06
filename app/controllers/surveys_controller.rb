712076b43a32a5b440fab173dc4f6b386b008c65
Johnny Loi <theaznshumai@gmail.com>
2014-02-06 01:09:21 -0800

    Fixed rateableSkill

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -6,7 +6,6 @@ class SurveysController < ApplicationController
 
     def create
         @survey = Survey.new(new_survey_params)
-        @survey.rateable_skills.build(params[:survey][:rateable_skill])
         if @survey.save
                 redirect_to root_url
         end
@@ -15,7 +14,7 @@ class SurveysController < ApplicationController
     private
 
     def new_survey_params
-        params.require(:survey).permit(:id, :name, rateable_skill_attributes: [:id, :desc]) 
+        params.require(:survey).permit(:id, :name, rateable_skills_attributes: [:id, :desc, :_destroy]) 
     end
 
 end
