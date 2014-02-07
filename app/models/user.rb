c0acdf88d2857da951170ba5ef4e09f2f6d487f4
Johnny Loi <theaznshumai@gmail.com>
2014-02-06 19:36:52 -0800

    Removed Roles Added admin attribute

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,21 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_one :role, dependent: :destroy
   has_many :skills, dependent: :destroy
-  before_create :build_default_role
-
-  def build_default_role
-      build_role
-      true
-  end
-
-  def has_role?(*role_names)
-      self.roles.where(:description => role_names).present?
-  end
-
-  def is_admin?
-      self.has_role?('admin')
-  end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140206031948) do
+ActiveRecord::Schema.define(version: 20140207033420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,13 +30,6 @@ ActiveRecord::Schema.define(version: 20140206031948) do
     t.datetime "updated_at"
   end
 
-  create_table "roles", force: true do |t|
-    t.string   "description", default: "client"
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "skills", force: true do |t|
     t.string   "name"
     t.integer  "user_id"
@@ -51,18 +44,19 @@ ActiveRecord::Schema.define(version: 20140206031948) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin",                  default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
