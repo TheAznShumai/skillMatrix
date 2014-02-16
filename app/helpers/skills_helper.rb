5ebea345f2ed6f83fcfdd3e7ea5706eafdf8a230
Johnny Loi <theaznshumai@gmail.com>
2014-02-15 19:40:38 -0800

    fixed a skills_id bug

--- app/helpers/skills_helper.rb
+++ app/helpers/skills_helper.rb
@@ -1,8 +1,10 @@
 module SkillsHelper
     def add_to_skills(skill_names)
+        @skills = []
         skill_names.each do |f|
-            current_user.skills.where(:name => f.name).first_or_create(
+            @skills << current_user.skills.where(:name => f.name).first_or_create(
                 :name => f.name)
         end
+        return @skills
     end
 end
