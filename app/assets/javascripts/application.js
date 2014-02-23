6f993ff18ba201f7006b197c102cea10525adb32
Johnny Loi <theaznshumai@gmail.com>
2014-02-22 18:50:50 -0800

    fixed datatables init

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,5 @@
 //= require jquery_ujs
 //= require bootstrap
 //= require dataTables/jquery.dataTables
-//= require dataTables/jquery.dataTables.bootstrap
+//= require dataTables/jquery.dataTables.bootstrap3
 //= require_tree .
