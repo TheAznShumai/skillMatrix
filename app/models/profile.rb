723a43cd4d887b5ec2ab0ee4b83773405809b836
Johnny Loi <theaznshumai@gmail.com>
2014-02-28 20:31:04 -0800

    Added avatar attachment

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,13 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
 
+  has_attached_file :avatar, :styles => { :small => "160x160>", 
+                                          :thumb => "100x100>" }, 
+                                          :default_url => "/images/:style/missing.png"
+
+  validates_attachment_size :avatar, :less_than => 5.megabytes
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
   validates :first_name, :last_name,
             presence: true
 
