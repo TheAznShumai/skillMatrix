f36d9ab746b67901d8d4d744a96c9004227ee692
Johnny Loi <theaznshumai@gmail.com>
2014-03-03 17:22:40 -0800

    Updating the look and feel for survey index

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,6 +1,6 @@
 class Survey < ActiveRecord::Base
   has_many :rateable_skills
-  has_attached_file :icon, :styles => { :ipod => "120x120>"},
+  has_attached_file :icon, :styles => { :small => "120x120>"},
                            :default_url => "document.png"
 
   validates_attachment_size :icon, :less_than => 5.megabytes
