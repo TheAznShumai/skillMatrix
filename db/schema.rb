69c8e1467bf5bafda058ea53564a7fb794c1e414
Johnny Loi <theaznshumai@gmail.com>
2014-03-14 21:36:35 -0700

    added answer model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140315042749) do
+ActiveRecord::Schema.define(version: 20140315043457) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: true do |t|
+    t.integer  "question_id"
+    t.string   "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "attempts", force: true do |t|
     t.integer  "user_id"
