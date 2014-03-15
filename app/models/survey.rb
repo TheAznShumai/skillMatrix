0805624724eb236439efd713c5b180fe4fc0c2e1
Johnny Loi <theaznshumai@gmail.com>
2014-03-14 20:55:47 -0700

    added questions_field to survey

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -7,6 +7,8 @@ class Survey < ActiveRecord::Base
   validates_attachment_size :icon, :less_than => 5.megabytes
   validates_attachment_content_type :icon, :content_type => /\Aimage\/.*\Z/
   validates_attachment_file_name :icon, :matches => [/png\Z/, /jpe?g\Z/]
+  
+  accepts_nested_attributes_for :questions, allow_destroy: true
   accepts_nested_attributes_for :rateable_skills, allow_destroy: true
 
   validates :name, presence: true
