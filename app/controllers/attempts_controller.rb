442e3ea8dc87e533090cc50c815517a1482813a2
Johnny Loi <theaznshumai@gmail.com>
2014-03-16 00:04:59 -0700

    questions and answers in the attempts new

--- app/controllers/attempts_controller.rb
+++ app/controllers/attempts_controller.rb
@@ -1,13 +1,19 @@
 class AttemptsController < ApplicationController
   def new
     @attempt = Attempt.new
+    @attempt.answers.build
+
     @survey = Survey.find(params[:survey_id])
+
     rateable_skills = @survey.rateable_skills.where(true).pluck(:name)
     skill_ids = Skill.add_to_skills(current_user, rateable_skills)
+
     @ratings = current_user.ratings.joins(:user_skill).find(
                                           :all, :conditions => 
                                         { :user_skills => 
                                         { :skill_id =>  skill_ids }}) 
+
+    @questions = @survey.questions.where(true)
   end
   
   def create
@@ -17,7 +23,8 @@ class AttemptsController < ApplicationController
   private
 
   def new_attempt_params
-    params.require(:attempt).permit(:user_id, :survey_id)
+    params.require(:attempt).permit(:user_id, :survey_id, 
+                                    :answers_attributes => [:text])
   end
 
 end
