720d5c9e826080488fdab99fce6a3980b5535b92
Johnny Loi <theaznshumai@gmail.com>
2014-03-15 17:25:43 -0700

    Added basic methods

--- app/controllers/attempts_controller.rb
+++ app/controllers/attempts_controller.rb
@@ -1,2 +1,17 @@
 class AttemptsController < ApplicationController
+  def new
+    @attempt = Attempt.new
+  end
+  
+  def create
+    @attempt = Attempt.new(new_attempt_params)
+  end
+
+  private
+
+  def new_attempt_params
+    params.require(:attempt).permit(:user_id, :survey_id)
+  end
+
 end
+
