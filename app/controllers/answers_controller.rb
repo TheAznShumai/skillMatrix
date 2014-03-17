ed885620780caa63df84d44ceae0284f0c8ca4c9
Johnny Loi <theaznshumai@gmail.com>
2014-03-16 17:58:08 -0700

    basic answer controller create method

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,20 @@
 class AnswersController < ApplicationController
+  def new
+    @answer = Answer.new
+  end
+
+  def create
+    @answer = Answer.new(new_answer_params)
+    if @answer.save
+      redirect_to_root
+    end
+  end
+
+  private
+
+  def new_answer_params
+    params.require(:answer).permit(:text)
+  end
+
 end
+
