f9d33da8fd6d7a1dd93865a06ba61e30cf2894a8
Johnny Loi <theaznshumai@gmail.com>
2014-03-24 14:00:56 -0700

    preping for a admin edit survey page

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,25 +1,39 @@
 class SurveysController < ApplicationController
     def new
-        @survey = Survey.new
-        @survey.rateable_skills.build
+     @survey = Survey.new
+     @survey.rateable_skills.build
 
-        format_response
+     format_response
     end
 
     def create
-        @survey = Survey.new(new_survey_params)
-        flash.clear
-        if @survey.save
-          flash[:success] = "Survey \"#{@survey.name}\" has been saved"
-          format_response
-        else
-          flash[:error] = @survey.errors.full_messages.uniq.join("\n")
-        end
+     @survey = Survey.new(new_survey_params)
+     flash.clear
+     if @survey.save
+       flash[:success] = "Survey \"#{@survey.name}\" has been saved"
+       format_response
+     else
+       flash[:error] = @survey.errors.full_messages.uniq.join("\n")
+     end
+    end
+
+    def edit
+      @survey = Survey.find(params[:id])
+    end
+
+    def update
+      @survey = Survey.find(params[:id])
+      if @survey.update_attribute(new_survey_params)
+        format_response
+      else
+        flash[:error] = @survey.errors.full_messages.uniq.join("\n")
+      end
     end
 
     def index
       @surveys = Survey.where(true).includes(:tags)
       @survey_tags = ActsAsTaggableOn::Tag.joins(:taggings).where("taggings.taggable_type = ?", "Survey").pluck(:name)
+      format_response
     end
 
     private
@@ -32,7 +46,7 @@ class SurveysController < ApplicationController
     end
 
     def new_survey_params
-        params.require(:survey).permit(
+      params.require(:survey).permit(
                        :name, :tag_list, :icon,
                        :rateable_skills_attributes => [:name, :tag_list, :_destroy],
                        :questions_attributes => [:text, :_destroy])
