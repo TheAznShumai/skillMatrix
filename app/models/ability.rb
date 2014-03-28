9d85cf806c95559b348bea9e6ded63aa6a5324ec
Johnny Loi <theaznshumai@gmail.com>
2014-03-27 17:23:03 -0700

    init cancan perm settings

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,15 +5,12 @@ class Ability
     user ||= User.new
     if user.admin?
       can :manage, :all
-    elsif user_signed_in?
-      cannot :manage, :all
-      can :manage, :profile => {:user_id => user.id}
-      can :manage, :skill => {:user_id => user.id}
-      can :manage, :attempt => {:user_id => user.id}
-      can :read, :static_pages
     else
       cannot :manage, :all
       can :read, :static_pages
+      can :manage, Profile, :user_id => user.id
+      can :read, Survey
+      can :manage, Attempt, :user_id => user.id
     end
   end
 end
