3b4144599125d02bc10e431e31a84157253575e4
Johnny Loi <theaznshumai@gmail.com>
2014-04-03 21:12:50 -0700

    took out unused scope

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,7 +3,4 @@ class Question < ActiveRecord::Base
   belongs_to :survey
 
   validates :text, presence: true
-
-  scope :from_survey_attempt, ->(survey_id, attempt_id) { joins(:survey => :attempts).where(:survey_id => survey_id,
-                                                                                     :attempts => {:id => attempt_id}) }
 end
