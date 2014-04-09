65993af795c7f146b2bffa722adff8a8643f6e95
Johnny Loi <theaznshumai@gmail.com>
2014-04-09 11:19:24 -0700

    added the manager/employee relationship in User

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,9 @@ class User < ActiveRecord::Base
   has_many :attempts, :dependent => :destroy
   has_many :surveys, :through => :attempts
 
+  belongs_to :manager, :class_name => 'User'
+  has_many :employees, :class_name => 'User', :foreign_key => 'manager_id'
+
   accepts_nested_attributes_for :profile
 
   # HTML 5 Validates on these values below
