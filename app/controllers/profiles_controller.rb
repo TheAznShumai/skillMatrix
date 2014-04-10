6ceb109c281f2127fff17980300fa520eac9278b
Johnny Loi <theaznshumai@gmail.com>
2014-04-10 02:58:15 -0700

    moved the edit profile logic into user edit logic and called user edit to modify both the user and the nested profile table

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,38 +1,13 @@
 class ProfilesController < ApplicationController
   authorize_resource
-  def edit
-    @profile = Profile.where(:id => params[:id]).first
-  end
 
   def show
     @profile = Profile.where(:id => params[:id]).first
-    @user = User.joins(:profile).where(:profiles => {:id => params[:id]}).includes(:attempts => :survey).first
+    @user = User.joins(:profile).where(:profiles => {:id => params[:id]})\
+      .includes(:attempts => :survey).first
     respond_to do |format|
       format.html
       format.json {render :json => @profile}
     end
   end
-
-  def update
-    @profile = Profile.where(:id => params[:id]).first
-    if @profile.update_attributes!(profile_params)
-      respond_to do |format|
-        format.html { redirect_to( @profile)}
-        format.json { render :json => @profile}
-      end
-    else
-      respond_to do |format|
-        format.html { render :action  => :edit } # edit.html.erb
-        format.json { render :nothing =>  true }
-      end
-    end
-  end
-
-  private
-
-  def profile_params
-    params.require(:profile).permit(:first_name, :last_name, :title, :office_phone, :cell_phone,
-                                    :department, :avatar)
-  end
-
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,22 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.where(:id => params[:id]).first
+  end
+
+  def update
+    @user = User.where(:id => params[:id]).first
+    if @user.update_attributes!(user_params)
+      respond_to do |format|
+        format.html {redirect_to @user.profile}
+      end
+    else
+      #TODO do a flash
+      render nothing: true
+    end
+  end
+
   def compose_email
     # Temp mail compose controller for demo purposes
     # TODO - create a mailbox model with proper validations and handlers
@@ -25,6 +41,14 @@ class UsersController < ApplicationController
   end
 
   private
+
+  def user_params
+    params.require(:user).permit(:manager_id,
+                                 :profile_attributes =>
+                                [:first_name, :last_name,
+                                 :title, :office_phone, :cell_phone,
+                                 :department, :avatar])
+  end
 
   def new_email_params
     params.require(:email).permit(:to, :cc, :bcc, :subject, :body)
