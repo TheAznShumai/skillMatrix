3defad949b4a1322cfa025af606844660fcdd913
Johnny Loi <theaznshumai@gmail.com>
2014-04-11 20:15:24 -0700

    removed phone validation, will need to come up with a good solution on the different phone combinations possible

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -13,11 +13,6 @@ class Profile < ActiveRecord::Base
   validates :first_name, :last_name,
             presence: true
 
-  validates :office_phone, :cell_phone,
-            allow_blank: true,
-            presence: {format: { with: /\d{3}-\d{3}-\d{4}/,
-                                 message: "Incorrect Phone Format" }}
-
   def full_name
     "#{first_name} #{last_name}"
   end
