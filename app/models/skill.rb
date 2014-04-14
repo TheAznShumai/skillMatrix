0ccfa0e67c740043a5649cdabb21ea6e5fb7617c
Johnny Loi <theaznshumai@gmail.com>
2014-04-14 16:23:59 -0700

    added unique validation on skill.name

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -4,6 +4,7 @@ class Skill < ActiveRecord::Base
     has_many :surveys, :through => :survey_skills
     has_many :users, :through => :user_skills
     validates :name, presence: true
+    validates :name, uniqueness: true
 
     acts_as_taggable
 
