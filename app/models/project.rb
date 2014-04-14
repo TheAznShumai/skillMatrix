5f20262bf0cf30173555da9475fbb26b39449ae9
Johnny Loi <theaznshumai@gmail.com>
2014-04-14 14:44:42 -0700

    added STATUS array and comments

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,5 +2,11 @@ class Project < ActiveRecord::Base
   belongs_to :user
 
   # Status Enum for collection_select
-  STATUS = %w[Closed Open In-Progress Planning Suspended]
+  # Open = 0
+  # Planning = 1
+  # In-Progress = 2
+  # Suspended = 3
+  # Closed = 4
+
+  STATUS = %w[Open Planning In-Progress Suspended Closed]
 end
