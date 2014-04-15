c626dadeba52608f1fc3cc8377f7c14e90a08ddb
Johnny Loi <theaznshumai@gmail.com>
2014-04-14 18:02:32 -0700

    create is now defined

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,10 +1,17 @@
 class ProjectsController < ApplicationController
-  load_and_authorize_resource
+  load_and_authorize_resource :param_method => :new_project_params
 
   def new
   end
 
   def create
+    flash.clear
+    if @project.save
+      flash[:success] = "Project \"#{@project.name}\" has been saved"
+      redirect_to projects_path
+    else
+      flash[:error] = @project.errors.full_messages.uniq.join("\n")
+    end
   end
 
   def index
@@ -21,4 +28,9 @@ class ProjectsController < ApplicationController
 
   def destroy
   end
+
+  def new_project_params
+    params.require(:project).permit(:name, :description, :project_manager_id, :status_id, :department)
+  end
+
 end
