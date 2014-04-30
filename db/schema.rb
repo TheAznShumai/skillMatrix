8ee7290faf335152613df723955a413996df2f50
Johnny Loi <theaznshumai@gmail.com>
2014-04-29 17:57:40 -0700

    corrected a description field (from string to text)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140414234954) do
+ActiveRecord::Schema.define(version: 20140430004955) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,7 +49,7 @@ ActiveRecord::Schema.define(version: 20140414234954) do
 
   create_table "projects", force: true do |t|
     t.string   "name"
-    t.string   "description"
+    t.text     "description"
     t.integer  "project_manager_id"
     t.string   "department"
     t.integer  "status_id"
